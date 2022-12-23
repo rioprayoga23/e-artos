3e2e433914c133adc5621676115b3d645d192a68
Rio Prayoga <yoprayogap@gmail.com>
2022-12-23 23:45:57 +0700

    add landing page

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2D1B69",
+        secondary: "#E5E5E5",
+        third: "#473AD10F",
+      },
+      extend: {
+        fontFamily: {
+          sans: ["Nunito Sans"],
+        },
+      },
+    },
+    screens: {
+      tripleXl: { min: "1441px" },
+      xxl: { max: "1440px" },
+
+      xl: { max: "1279px" },
+
+      lg: { max: "1023px" },
+
+      md: { max: "767px" },
+
+      sm: { max: "639px" },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+  },
+};
