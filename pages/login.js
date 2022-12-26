8e779e6d3c66f542923a9cca709ddd5fe57b4279
Rio Prayoga <yoprayogap@gmail.com>
2022-12-26 21:40:06 +0700

    add formik for validation in login

--- pages/login.js
+++ pages/login.js
@@ -3,6 +3,23 @@ import React, { useState } from "react";
 import { Mail, Lock, EyeOff, Eye } from "react-feather";
 import MainLayoutAuth from "../components/layouts/MainLayoutAuth";
 
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import YupPassword from "yup-password";
+YupPassword(Yup); // extend yup
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .password()
+    .min(8, "Min length 8")
+    .minLowercase(1, "Min Lowecase 1")
+    .minUppercase(1, "Min Uppercase 1")
+    .minSymbols(1, "Min Symbols 1")
+    .minNumbers(1, "Min Numbers 1")
+    .required("Required"),
+});
+
 export default function Login() {
   const [type, setType] = useState("password");
 
@@ -10,10 +27,14 @@ export default function Login() {
     type === "password" ? setType("text") : setType("password");
   };
 
+  const doLogin = (value) => {
+    console.log(value);
+  };
+
   return (
     <MainLayoutAuth title="Login">
       <>
-        <article className="flex w-[40%] md:w-full lg:w-full">
+        <article className="flex w-[40%] md:w-full lg:w-full overflow-y-scroll h-screen">
           <div className="px-20 pt-16 md:px-9 lg:px-24 xl:pt-6">
             <h1 className="text-2xl font-semibold mb-6 hidden lg:block">
               Login
@@ -27,44 +48,87 @@ export default function Login() {
               wherever you are. Desktop, laptop, mobile phone? we cover all of
               that for you!
             </p>
-            <div className="relative mt-14">
-              <Mail className="absolute" style={{ color: "#A9A9A999" }} />
-              <input
-                type="email"
-                className="w-full border-b-2 focus:outline-none px-12 pb-2"
-                placeholder="Enter your e-mail"
-              />
-            </div>
-            <div className="relative mt-14 ">
-              <Lock className="absolute" style={{ color: "#A9A9A999" }} />
-              <input
-                type={type}
-                className="w-full border-b-2 focus:outline-none px-12 pb-2"
-                placeholder="Enter your password"
-              />
-              {type === "password" ? (
-                <EyeOff
-                  className="absolute right-0 top-1 cursor-pointer"
-                  style={{ color: "#A9A9A999" }}
-                  onClick={showPassword}
-                />
-              ) : (
-                <Eye
-                  className="absolute right-0 top-1 cursor-pointer"
-                  style={{ color: "#A9A9A999" }}
-                  onClick={showPassword}
-                />
-              )}
-            </div>
-            <div className="flex justify-end mt-5">
-              <Link href="/forgot-password" className="text-primary">
-                Forgot password?
-              </Link>
-            </div>
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              validationSchema={LoginSchema}
+              onSubmit={(value) => doLogin(value)}
+            >
+              {({ errors, touched, dirty }) => (
+                <Form>
+                  <div className="relative mt-14">
+                    <Mail className="absolute" style={{ color: "#A9A9A999" }} />
+                    <Field
+                      type="email"
+                      name="email"
+                      className={`w-full border-b-2 focus:outline-none px-12 pb-2 ${
+                        errors.email && touched.email && "border-red-500"
+                      } ${
+                        !errors.email && touched.email && "border-purple-500"
+                      }`}
+                      placeholder="Enter your e-mail"
+                    />
+                    {errors.email && touched.email && (
+                      <label className="label">
+                        <span className="label-text-alt text-red-500">
+                          {errors.email}
+                        </span>
+                      </label>
+                    )}
+                  </div>
+                  <div className="relative mt-14 ">
+                    <Lock className="absolute" style={{ color: "#A9A9A999" }} />
+                    <Field
+                      type={type}
+                      name="password"
+                      className={`w-full border-b-2 focus:outline-none px-12 pb-2 ${
+                        errors.password && touched.password && "border-red-500"
+                      } ${
+                        !errors.password &&
+                        touched.password &&
+                        "border-purple-500"
+                      }`}
+                      placeholder="Enter your password"
+                    />
+                    {type === "password" ? (
+                      <EyeOff
+                        className="absolute right-0 top-1 cursor-pointer"
+                        style={{ color: "#A9A9A999" }}
+                        onClick={showPassword}
+                      />
+                    ) : (
+                      <Eye
+                        className="absolute right-0 top-1 cursor-pointer"
+                        style={{ color: "#A9A9A999" }}
+                        onClick={showPassword}
+                      />
+                    )}
+                  </div>
+                  {errors.password && touched.password && (
+                    <label className="label">
+                      <span className="label-text-alt text-red-500">
+                        {errors.password}
+                      </span>
+                    </label>
+                  )}
+                  <div className="flex justify-end mt-5">
+                    <Link href="/forgot-password" className="text-primary">
+                      Forgot password?
+                    </Link>
+                  </div>
 
-            <button className="btn bg-primary w-full mt-10 hover:bg-primary">
-              Login
-            </button>
+                  <button
+                    type="submit"
+                    disabled={!dirty}
+                    className="btn bg-primary w-full mt-10 hover:bg-primary"
+                  >
+                    Login
+                  </button>
+                </Form>
+              )}
+            </Formik>
 
             <div className="text-center mt-5 pb-10">
               Don’t have an account?
